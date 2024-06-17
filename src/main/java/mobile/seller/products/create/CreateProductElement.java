6f72b3298b1aaf55de8eb1f3716f5acacce389ca
Admin <thangnguyendinh1995@gmail.com>
2024-06-17 16:44:47 +0700

    Add function for create without variation product.

--- src/main/java/mobile/seller/products/create/CreateProductElement.java
+++ src/main/java/mobile/seller/products/create/CreateProductElement.java
@@ -1,14 +1,21 @@
 package mobile.seller.products.create;
 
+import org.openqa.selenium.By;
+
 import static utilities.environment.EnvironmentInformation.goSELLERBundleId;
 
 public class CreateProductElement {
     String rsId_btnSelectImage = "%s:id/rlSelectImages".formatted(goSELLERBundleId);
+    By loc_dlgSelectImage_lstImages = By.xpath("//*[@*='%s:id/tvSelectIndex']".formatted(goSELLERBundleId));
+    String rsId_dlgSelectImage_btnSave = "%s:id/fragment_choose_photo_dialog_btn_choose".formatted(goSELLERBundleId);
     String rsId_txtProductName = "%s:id/edtProductName".formatted(goSELLERBundleId);
     String rsId_btnProductDescription = "%s:id/tvProductDescription".formatted(goSELLERBundleId);
-    String rsId_txtWithoutVariationListingPrice = "%s:id/edtProductOrgPrice".formatted(goSELLERBundleId);
-    String rsId_txtWithoutVariationSellingPrice = "%s:id/edtProductNewPrice".formatted(goSELLERBundleId);
-    String rsId_txtWithoutVariationCostPrice = "%s:id/edtProductCostPrice".formatted(goSELLERBundleId);
+    String rsId_dlgProductDescription_txtContent = "editor";
+    String rsId_dlgProductDescription_btnSave = "com.mediastep.GoSellForSeller.STG:id/ivActionBarIconRight";
+    String rsId_sctPrice = "%s:id/clProductPriceContainer".formatted(goSELLERBundleId);
+    By loc_txtWithoutVariationListingPrice = By.xpath("//*[@*= '%s:id/edtProductOrgPrice']//*[@* = '%s:id/edtPriceCustom']".formatted(goSELLERBundleId, goSELLERBundleId));
+    By loc_txtWithoutVariationSellingPrice = By.xpath("//*[@*= '%s:id/edtProductNewPrice']//*[@* = '%s:id/edtPriceCustom']".formatted(goSELLERBundleId, goSELLERBundleId));
+    By loc_txtWithoutVariationCostPrice = By.xpath("//*[@*= '%s:id/edtProductCostPrice']//*[@* = '%s:id/edtPriceCustom']".formatted(goSELLERBundleId, goSELLERBundleId));
     String rsId_ddvSelectedVAT = "%s:id/tvVAT".formatted(goSELLERBundleId);
     String rsId_txtWithoutVariationSKU = "%s:id/edtSKU".formatted(goSELLERBundleId);
     String rsId_txtWithoutVariationBarcode = "%s:id/edtProductBarcode".formatted(goSELLERBundleId);
