ad7066a8eeca50115831238290c4071302b7c6de
Admin <thangnguyendinh1995@gmail.com>
2024-06-20 10:20:00 +0700

    Add create product/product management activity

--- src/main/java/utilities/environment/EnvironmentInformation.java
+++ src/main/java/utilities/environment/EnvironmentInformation.java
@@ -2,7 +2,9 @@ package utilities.environment;
 
 public class EnvironmentInformation {
     public final static String goSELLERBundleId = "com.mediastep.GoSellForSeller.STG";
-    public final static String goSELLERActivity = "com.mediastep.gosellseller.modules.credentials.login.LoginActivity";
+    public final static String goSELLERLoginActivity = "com.mediastep.gosellseller.modules.credentials.login.LoginActivity";
+    public final static String goSELLERCreateProductActivity = "com.mediastep.gosellseller.modules.upload_product.CreateProductActivity";
+    public final static String goSELLERProductManagementActivity = "com.mediastep.gosellseller.modules.product_management.ProductManagementActivity";
     public final static String goBuyerBundleId = "com.mediastep.shop0018";
     public final static String goBuyerActivity = "com.mediastep.shop0018.ui.modules.splash.SplashScreenActivity";
 }
