5adb1115e0f429189dcf3a43baf6f54c222bc509
Admin <thangnguyendinh1995@gmail.com>
2024-06-21 17:42:38 +0700

    break environment for goSELLER/goBUYER

--- src/main/java/mobile/seller/products/child_screen/select_image_popup/SelectImageElement.java
+++ src/main/java/mobile/seller/products/child_screen/select_image_popup/SelectImageElement.java
@@ -2,7 +2,7 @@ package mobile.seller.products.child_screen.select_image_popup;
 
 import org.openqa.selenium.By;
 
-import static utilities.environment.EnvironmentInformation.goSELLERBundleId;
+import static utilities.environment.goSELLEREnvironment.goSELLERBundleId;
 
 public class SelectImageElement {
     By loc_lstImages = By.xpath("//*[@*='%s:id/tvSelectIndex']".formatted(goSELLERBundleId));
