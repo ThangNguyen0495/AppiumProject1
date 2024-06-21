07593b70087c7603be267663b301fd958817f884
Admin <thangnguyendinh1995@gmail.com>
2024-06-21 09:35:09 +0700

    Update local test.

--- src/test/java/seller/AppiumTest.java
+++ src/test/java/seller/AppiumTest.java
@@ -1,44 +1,35 @@
 package seller;
 
 import io.appium.java_client.AppiumDriver;
+import mobile.seller.home.HomeElement;
 import mobile.seller.home.HomeScreen;
 import mobile.seller.login.LoginScreen;
+import mobile.seller.products.create_product.CreateProductScreen;
 import org.openqa.selenium.By;
 import org.testng.annotations.Test;
 import utilities.commons.UICommonMobile;
 import utilities.driver.InitAppiumDriver;
+import utilities.model.sellerApp.login.LoginInformation;
 
-import java.net.MalformedURLException;
-
-import static mobile.seller.home.HomeElement.QuickActions.addProduct;
-import static utilities.environment.EnvironmentInformation.*;
-
-public class AppiumTest {
-    private final static String udid = "192.168.168.101:5555";
-    private final static String url = "http://127.0.0.1:4723/wd/hub";
-
-    private AppiumDriver getSellerDriver() throws MalformedURLException {
-        return new InitAppiumDriver().getAppiumDriver(udid, "ANDROID", goSELLERBundleId, goSELLERActivity, url);
-    }
-
-    private AppiumDriver getBuyerDriver() throws MalformedURLException {
-        return new InitAppiumDriver().getAppiumDriver(udid, "ANDROID", goBuyerBundleId, goBuyerActivity, url);
-    }
+public class AppiumTest extends BaseTest {
 
     String feature_id = "tm-product-service-default-layout";
     By xpath = By.xpath("//android.view.View[@resource-id=\"tm-product-service-default-layout\"]/android.view.View");
 
     @Test()
     void t() throws Exception {
-        AppiumDriver driver = getSellerDriver();
-        new LoginScreen(driver).performLogin("stgaboned@nbobd.com", "Abc@12345");
-        new HomeScreen(driver).navigate(addProduct);
+        AppiumDriver driver = new InitAppiumDriver().getSellerDriver("192.168.168.102:5555");
+        new LoginScreen(driver).performLogin(new LoginInformation("stgaboned@nbobd.com", "Abc@12345"));
+        new HomeScreen(driver).navigate(HomeElement.QuickActions.addProduct);
+        new CreateProductScreen(driver).getManageByIMEI(false)
+                .navigateToCreateProductScreen()
+                .createProductWithVariation(5);
 //        driver.quit();
     }
 
     @Test()
     void t2() throws Exception {
-        AppiumDriver driver = getBuyerDriver();
+//        AppiumDriver driver = getBuyerDriver();
         UICommonMobile commonMobile = new UICommonMobile(driver);
         commonMobile.getElement(feature_id);
         commonMobile.click(xpath, 7);
