81e70fcb120feea334eaf36a14d26f60ad105b0c
admin <thangnguyendinh1995@gmail.com>
2024-08-16 11:56:31 +0700

    update workflow

--- scripts/RunAppiumServer.sh
+++ scripts/RunAppiumServer.sh
@@ -2,8 +2,9 @@
 set -ex
 npm install -g appium@next
 appium driver install xcuitest
-appium driver install espresso
-appium driver install flutter
+appium driver install uiautomator2
+#appium driver install espresso
+#appium driver install flutter
 
 appium -v
 appium --log appium.log &>/dev/null &
