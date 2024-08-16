7b1944bbc6aa1d4a1413d2e8d26e7a8de02fb7c9
admin <thangnguyendinh1995@gmail.com>
2024-08-16 17:25:09 +0700

    run all testcase

--- src/main/java/utilities/recording/ScreenRecording.java
+++ src/main/java/utilities/recording/ScreenRecording.java
@@ -4,7 +4,6 @@ import io.appium.java_client.screenrecording.CanRecordScreen;
 import lombok.SneakyThrows;
 import org.openqa.selenium.WebDriver;
 import org.testng.ITestResult;
-import utilities.data.DataGenerator;
 
 import java.io.File;
 import java.nio.file.Files;
@@ -19,16 +18,16 @@ public class ScreenRecording {
     }
 
     @SneakyThrows
-    public static void stopRecording(WebDriver driver, ITestResult result){
+    public static void stopRecording(WebDriver driver, ITestResult result) {
         // Stop screen recording and get the recorded video
         String video = ((CanRecordScreen) driver).stopRecordingScreen();
 
         // Save the video to a file when test is failed
-        if (!result.isSuccess()) {
-            java.util.Base64.Decoder decoder = java.util.Base64.getDecoder();
-            byte[] decodedBytes = decoder.decode(video);
-            Path path = Paths.get(new DataGenerator().getPathOfFolderInResourceRoot("recording_video") + File.separator + "%s.mp4".formatted(result.getName()));
-            Files.write(path, decodedBytes);
-        }
+//        if (!result.isSuccess()) {
+        java.util.Base64.Decoder decoder = java.util.Base64.getDecoder();
+        byte[] decodedBytes = decoder.decode(video);
+        Path path = Paths.get(System.getProperty("user.dir") + "/recording_video/" + File.separator + "%s.mp4".formatted(result.getName()));
+        Files.write(path, decodedBytes);
+//        }
     }
 }
