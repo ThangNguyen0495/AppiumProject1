68052cb3bf5d00796d3ec06291df05fd3dfc9787
admin <thangnguyendinh1995@gmail.com>
2024-08-16 17:36:14 +0700

    run all testcase

--- src/main/java/utilities/recording/ScreenRecording.java
+++ src/main/java/utilities/recording/ScreenRecording.java
@@ -2,6 +2,7 @@ package utilities.recording;
 
 import io.appium.java_client.screenrecording.CanRecordScreen;
 import lombok.SneakyThrows;
+import org.apache.logging.log4j.LogManager;
 import org.openqa.selenium.WebDriver;
 import org.testng.ITestResult;
 
@@ -26,6 +27,10 @@ public class ScreenRecording {
 //        if (!result.isSuccess()) {
         java.util.Base64.Decoder decoder = java.util.Base64.getDecoder();
         byte[] decodedBytes = decoder.decode(video);
+        // Create recording_video if that not available
+        File theDir = new File("./recording_video/");
+        if (!theDir.exists())
+            LogManager.getLogger().info(theDir.mkdirs() ? "Create folder 'recording_video' folder" : "Can not create 'recording_video' folder");
         Path path = Paths.get(System.getProperty("user.dir") + "/recording_video/" + File.separator + "%s.mp4".formatted(result.getName()));
         Files.write(path, decodedBytes);
 //        }
